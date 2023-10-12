cacee459cf70bbda0e969147d550bf7845068d57
avon8080 <evanjlalor@gmail,com>
2023-10-12 18:28:14 -0500

    midterm assignment

---
+++ RaceTrack/Cars/EleanorMustang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class EleanorMustang : RaceCar
+    
+    {
+        public EleanorMustang()
+        {
+            Name = "1967 Eleanor mustang";
+            TopSpeed = 100;
+        }
+       
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The natural sound of the {Name} is just screaming American muscle. ");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} slows down from {TopSpeed}. Ends in {Position}");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/RickyBobby.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class RickyBobby : Driver
+    {
+        public RickyBobby(RaceCar raceCar) : base(raceCar)
+        {
+            Name = "Ricky Bobby";
+            SkillLevel = 20;
+
+        }
+        public override void Drive()
+        {
+            Console.WriteLine($"{Name} throws it in gear. 5 seconds late. Yup, Flying Through The Air. This Is Not Good. ");
+        }
+    }
+}
